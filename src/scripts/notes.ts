d298fe3804d48a9f242836b13f8bb73d662f8280
amp-m9 <andrickposo3001@gmail.com>
2023-07-27 00:54:39 +0100

    tidy

--- src/scripts/notes.ts
+++
@@ -1,6 +0,0 @@
-mainCell.highlight.material.opacity = 0.5;
-let textPos = camera.worldToLocal(mainCell.mesh.position.clone());
-textPos.add(new THREE.Vector3(.8,.8,0));
-textPos = camera.localToWorld(textPos)
-bloodCellLabel.position.set(...textPos.toArray())
-addIfNotInScene(bloodCellLabel)
